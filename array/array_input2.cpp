78d588a41930f1e68e68afa729b4dd8e22fdb222
superchd <63406434+superchd@users.noreply.github.com>
2022-01-24 16:07:05 +0900

    using macro to use stlarray
    매크로를 써서, stlarray를 억지로 사용했으나, 반드시 array의 크기를 정해놓아햐는게 아쉽다

--- array/array_input2.cpp
+++ array/array_input2.cpp
@@ -1,21 +1,23 @@
 #include <cstdio>
 #include <iostream>
 #include <array>
+#define N 100
 
 using namespace std;
 
-unsigned int		n;
+int		n;
+array<int, N>   my_arr;
 
-	my_input(void){
+void	my_input(void){
 
 	int num;
 
 	cin >> n;
 	for (int i = 0; i < n; i++){
 		cin >> num;
+		my_arr[i] = num;
 	}
-
-return ;
+	return ;
 }
 
 int main(void){
