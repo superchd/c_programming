6dd8dd2eb76e78063f03f41c04c62e09d221ed4d
superchd <63406434+superchd@users.noreply.github.com>
2022-01-24 15:54:04 +0900

    Create array_input2.cpp

---
+++ array/array_input2.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+unsigned int		n;
+
+	my_input(void){
+
+	int num;
+
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> num;
+	}
+
+return ;
+}
+
+int main(void){
+	my_input();
+
+	return 0;
+
+
+
+}
